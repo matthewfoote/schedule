60605651e5b4112b4a63c16a838709ac24341c33
Matthew <matthewfoote1127@gmail.com>
2018-07-30 14:53:20 +0000

    built library course component with grid and library grid

--- src/components/home.js
+++ src/components/home.js
@@ -7,6 +7,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
+        
         <Library/>
         <Schedule/>
       </div>
---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                {/**/}
+                <div className="library-course __description">
+                    <label> Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a libero sed nunc suscipit suscipit sed vitae diam. Donec id felis id justo eleifend sagittis a et diam. Vestibulum condimentum pulvinar lacinia. Morbi lorem ante, euismod nec tempus quis, vehicula at nulla. Proin condimentum tempor urna.</p>
+                    </div>
+            </div>
+            )
+    }
+}
+
+export default LibraryCourse;
